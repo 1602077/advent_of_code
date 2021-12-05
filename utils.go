a6005b0ba44a16f02994863e239aefc59311767f
1602077 <62025739+1602077@users.noreply.github.com>
2021-12-05 16:21:14 +0000

    create utils package

---
+++ utils.go
@@ -0,0 +1,21 @@
+package utils
+
+func readInput(filename string)  []string {
+    file, err := os.Open(filename)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    sc := bufio.NewScanner(file)
+    lines := make([]string, 0)
+
+    for sc.Scan() {
+        lines = append(lines, sc.Text())
+    }
+
+    if err := sc.Err(); err != nil {
+        log.Fatal(err)
+    }
+    return lines
+}
