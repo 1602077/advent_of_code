232d0829ce25b1347f63da3423c1615fb9427411
1602077 <62025739+1602077@users.noreply.github.com>
2021-12-06 07:49:40 +0000

    first attempt at day_04

---
+++ day_04/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+    "strings"
+
+    "github.com/1602077/advent_of_code/utils"
+)
+
+func main() {
+    splitInputs("./input.txt")
+
+}
+
+type BingBoard struct {
+    grid map[int]
+}
+
+func splitInputs(filename string){
+    data := utils.ReadInput(filename)
+    numBoards := (len(data) - 1) / 6
+
+    fmt.Printf("Number of lines in dataset: %v.\n", len(data))
+    fmt.Printf("Number of boards in dataset: %v.\n", numBoards)
+
+    numberDraw := parseNumberDraw(data[0])
+
+    bingoGrids := []BingBoard{}
+
+}
+
+func parseNumberDraw(line string) []int16 {
+    calledNumStr := strings.Split(line, ",")
+    var numberDraw []int16
+    for _, value := range calledNumStr {
+        num, _ := strconv.ParseInt(value, 10, 16)
+        numberDraw = append(numberDraw, int16(num))
+    }
+    fmt.Printf("Numbers called: %v.\n", numberDraw)
+    return numberDraw
+}
