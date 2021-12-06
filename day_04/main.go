383ceab6bd67efa3052a7c21a1958b051cd8085f
1602077 <62025739+1602077@users.noreply.github.com>
2021-12-06 20:09:58 +0000

    add logic to read in bingo boards and number drawn

--- day_04/main.go
+++ day_04/main.go
@@ -4,8 +4,10 @@ import (
     "fmt"
     "strconv"
     "strings"
+    "io/ioutil"
+    "log"
 
-    "github.com/1602077/advent_of_code/utils"
+    // "github.com/1602077/advent_of_code/utils"
 )
 
 func main() {
@@ -13,24 +15,31 @@ func main() {
 
 }
 
-type BingBoard struct {
-    grid [][]int
+type bingoBoard struct {
+    grid []byte
     called [][]bool
 }
 
-func splitInputs(filename string){
-    data := utils.ReadInput(filename)
-    numBoards := (len(data) - 1) / 6
-
-    fmt.Printf("Number of lines in dataset: %v.\n", len(data))
-    fmt.Printf("Number of boards in dataset: %v.\n", numBoards)
-
-    numberDraw := parseNumberDraw(data[0])
-
-    bingoGrids := []BingBoard{}
-
+func readInputAsString(filename string) string {
+    content, err := ioutil.ReadFile("./input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    return string(content)
 }
 
+func splitInputs(filename string){
+    data := readInputAsString(filename)
+    sects := strings.Split(data, "\n\n")
+
+    numberDraw := parseNumberDraw(sects[0])
+    bingoBoards := parseAllBingoBoards(sects[1:])
+
+    fmt.Printf("Numbers called: %v.\n\n", numberDraw)
+    fmt.Printf("Bingo Boards 1 & 2: \n %v \n\n %v", bingoBoards[0], bingoBoards[1])
+}
+
+// Get called numbers from first line of `input.txt`
 func parseNumberDraw(line string) []int16 {
     calledNumStr := strings.Split(line, ",")
     var numberDraw []int16
@@ -38,6 +47,26 @@ func parseNumberDraw(line string) []int16 {
         num, _ := strconv.ParseInt(value, 10, 16)
         numberDraw = append(numberDraw, int16(num))
     }
-    fmt.Printf("Numbers called: %v.\n", numberDraw)
     return numberDraw
+}
+
+// extract a single bingo board from an input string
+func parseBingoBoard(input string) *bingoBoard {
+    b := bingoBoard{
+        grid: make([]byte, 25),
+    }
+    for i, field := range strings.Fields(input) {
+        num, _ := strconv.Atoi(field)
+        b.grid[i] = byte(num)
+    }
+    return &b
+}
+
+// Parse over all bingo boards in the dataset and return as a slice
+func parseAllBingoBoards(input []string) []bingoBoard {
+    var bingoBoards []bingoBoard
+    for _, part := range input {
+        bingoBoards = append(bingoBoards, *parseBingoBoard(part))
+    }
+    return bingoBoards
 }
