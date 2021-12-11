9cc6d86064cbcea49cbb9cfb02ec01ee86e969db
1602077 <62025739+1602077@users.noreply.github.com>
2021-12-11 12:39:02 +0000

    wireframe part 2

--- day_08/main.go
+++ day_08/main.go
@@ -16,21 +16,14 @@ type sigPattern struct {
 func main() {
     // part1("./input_test.txt")
     part1("./input.txt")
+    part2("./input.txt")
 }
 
 func part1(filename string) int {
     displaySignal := readInput(filename)
-
-    // count how many times 1, 4, 7, 8 appear in .output
+    /* 1: c and f; 4: b, c, d, f; 7: a, c, f; 8: a,b,c,d,e,f,g */
     count := 0
-    /*
-    1: c and f on
-    4: b, c, d, f
-    7: a, c, f
-    8: a,b,c,d,e,f,g
-    */
     for _, sig := range displaySignal {
-        fmt.Println(sig.output)
         for _, dig := range sig.output {
             length := len(dig)
             if length == 2 || length == 3 || length == 4 || length == 7 {
@@ -40,6 +33,29 @@ func part1(filename string) int {
     }
     fmt.Printf("PART 1: Number of times a 1, 4, 7 or 8 are displayed: %v.\n", count)
     return count
+}
+
+func part2(filename string) int {
+    displaySignal := readInput(filename)
+    total := 0
+    for _, sig := range displaySignal {
+        for _, dig := range sig.output {
+            switch {
+            case len(dig) == 2:
+                total += 1
+            case len(dig) == 3:
+                total += 7
+            case len(dig) == 4:
+                total += 4
+            case len(dig) == 7:
+                total += 8
+            default:
+                // TODO: Logic for cracking complex mappings
+            }
+        }
+    }
+    fmt.Printf("PART 2: Total of all output values: %v.\n", total)
+    return total
 }
 
 func readInput(filename string) []sigPattern {
