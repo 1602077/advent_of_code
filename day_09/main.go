d0038dfdb342933f47cc220cc616007ab047c18f
1602077 <62025739+1602077@users.noreply.github.com>
2021-12-12 14:05:55 +0000

    incorrect basin size logic; can't move L,R,U,D independtly

--- day_09/main.go
+++ day_09/main.go
@@ -11,38 +11,103 @@ import (
 )
 
 func main() {
-    part1("./input.txt")
+    // part1("./input.txt")
+    part2("./input_test.txt")
 }
 
-type lavaTube struct {
+type grid struct {
     heightMap [][]int
-    lowPoints []int
+    lpValue []int
+    lpPos [][]int
 }
 
 func part1(filename string) int {
     m := readInput(filename)
-
-    lT := lavaTube{heightMap: m, lowPoints: make([]int, 0)}
-
-    nRows := len(lT.heightMap)
-    nCols := len(lT.heightMap[0])
+    lT := grid{heightMap: m, lpValue: make([]int, 0), lpPos: make([][]int, 0)}
 
     r := 0
     for i := range lT.heightMap {
         for j := range lT.heightMap[0] {
             if isLowPoint(lT.heightMap, i, j) {
-                lT.lowPoints = append(lT.lowPoints, lT.heightMap[i][j])
+                lT.lpValue = append(lT.lpValue, lT.heightMap[i][j])
+                lT.lpPos = append(lT.lpPos, []int{i,j})
                 r += 1 + int(lT.heightMap[i][j])
             }
         }
     }
-    // Input file summary
-    // fmt.Printf("Input heightmap: \n%v.\n", lT.heightMap)
-    // fmt.Printf("Number of rows and columns: %v, %v.\n", nRows, nCols)
-    // fmt.Printf("Lavatube low points: %v.\n", lT.lowPoints)
-
     fmt.Printf("PART 1 Total risk score: %v.\n", r)
     return r
+}
+
+func part2(filename string) {
+    m := readInput(filename)
+    lT := grid{heightMap: m, lpValue: make([]int, 0), lpPos: make([][]int, 0)}
+
+    for i := range lT.heightMap {
+        for j := range lT.heightMap[0] {
+            if isLowPoint(lT.heightMap, i, j) {
+                lT.lpValue = append(lT.lpValue, lT.heightMap[i][j])
+                lT.lpPos = append(lT.lpPos, []int{i,j})
+            }
+        }
+    }
+    // TODO: calculate basin sizes
+    getBasinSizes(lT)
+
+}
+
+func getBasinSizes(lt grid) []int {
+    fmt.Println(lt.heightMap, "\n")
+    fmt.Println(lt.lpValue, "\n")
+
+    nRows := len(lt.heightMap)
+    nCols := len(lt.heightMap[0])
+
+    var basinSizes []int
+
+    for k, _ := range lt.lpValue {
+        pos := lt.lpPos[k]
+        fmt.Println(pos)
+        var basinSize int = 1
+        // iterate right
+        for i := pos[0]+1; i < nRows; i++ {
+            prev := lt.heightMap[i-1][pos[1]]
+            curr := lt.heightMap[i][pos[1]]
+            if curr > prev && curr != 9 {
+                basinSize++
+            }
+        }
+        // iterate left
+        for i := pos[0]-1; i >= 0; i-- {
+            prev := lt.heightMap[i+1][pos[1]]
+            curr := lt.heightMap[i][pos[1]]
+            if curr > prev && curr != 9 {
+                basinSize++
+            }
+        }
+        // iterate top
+        for j := pos[1] + 1; j < nCols; j++ {
+            prev := lt.heightMap[pos[0]][j-1]
+            curr := lt.heightMap[pos[0]][j]
+            if curr > prev && curr != 9 {
+                basinSize++
+            }
+        }
+        // iterate down
+        for j := pos[1] - 1; j >= 0; j-- {
+            prev := lt.heightMap[pos[0]][j+1]
+            curr := lt.heightMap[pos[0]][j]
+            if curr > prev && curr != 9 {
+                basinSize++
+            }
+        }
+        basinSizes = append(basinSizes, basinSize)
+    }
+    fmt.Printf("PART 2 Basin Sizes: %v.\n", basinSizes)
+    // TODO:  Sort basin sizes and return the product of them
+    var prod int
+    fmt.Printf("Part 2 Product of basin sizes: %v.\n", prod)
+    return basinSizes
 }
 
 func isLowPoint(matrix [][]int, i, j int) bool {
