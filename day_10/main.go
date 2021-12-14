53cf6b70a2e90ca11cf89e271cf9e5b6a8d31e80
1602077 <62025739+1602077@users.noreply.github.com>
2021-12-14 20:00:35 +0000

    complete day_10

--- day_10/main.go
+++ day_10/main.go
@@ -4,12 +4,13 @@ import (
     "fmt"
     "io/ioutil"
     "log"
+    "sort"
     "strings"
 )
 
 func main() {
-    // part1("./input_test.txt")
     part1("./input.txt")
+    part2("./input.txt")
 }
 
 type Stack []byte
@@ -35,7 +36,6 @@ func (s *Stack) Pop() (byte, bool) {
 
 func part1(filename string) int {
     input := readInput(filename)
-    // fmt.Printf("Input data:\n%v\n", input)
 
     points := map[byte]int{
         ')': 3,
@@ -56,8 +56,62 @@ func part1(filename string) int {
             p += points[l]
         }
     }
-    fmt.Printf("PART 1 Total syntax error score: %v.\n", p)
+    fmt.Printf("PART 1 Total syntax error score: %v\n", p)
     return p
+}
+
+func part2(filename string) int {
+    input := readInput(filename)
+
+    pairs := map[byte]byte{
+        ')': '(',
+        ']': '[',
+        '}': '{',
+        '>': '<',
+    }
+
+    var incomplete_lines []string
+
+    for _, line := range input {
+        corrupt, _ :=  isIllegal(line, pairs)
+        if corrupt == false {
+            incomplete_lines = append(incomplete_lines, line)
+        }
+    }
+
+    cScore := map[byte]int{
+        '(': 1,
+        '[': 2,
+        '{': 3,
+        '<': 4,
+    }
+
+    scores := make([]int, len(incomplete_lines))
+    for i, str := range incomplete_lines {
+        r := incomplete(str)
+        p := 0
+        for !r.IsEmpty() {
+            v, _ := r.Pop()
+            p = 5*p + cScore[v]
+        }
+        scores[i] = p
+    }
+
+    sort.Ints(scores)
+    fmt.Printf("PART 2 Total score to complete all lines: %v.\n", scores[len(scores)/2])
+    return scores[len(scores)/2]
+}
+
+func incomplete(line string) Stack {
+    var stack Stack
+    for i := range line {
+        if isOpen(line[i]) {
+            stack.Push(line[i])
+        } else {
+            stack.Pop()
+        }
+    }
+    return stack
 }
 
 func isOpen(str byte) bool {
