7d8a694bf287d40593e38633f85b443d4269d3d0
1602077 <62025739+1602077@users.noreply.github.com>
2021-12-20 21:53:04 +0000

    complete day_17

--- day_17/main.go
+++ day_17/main.go
@@ -8,31 +8,55 @@ import (
 
 func main() {
     part1("./input.txt")
+    part2("./input.txt")
 }
 
 func part1(filename string) int {
     _, y := readInput(filename)
-    var minY int
-    for _, v := range y {
-        if minY > v {
-            minY = v
-        }
-    }
-    maxY  := (minY + 1) * (minY) / 2
+    maxY  := (y[0] + 1) * (y[0]) / 2
     fmt.Printf("PART 1 Highest y position while still on trajectory: %v.\n", maxY)
     return maxY
 }
 
-func part2(filename string) {
-    _, y := readInput(filename)
-    var minY int
-    for _, v := range y {
-        if minY > v {
-            minY = v
+func part2(filename string) int {
+    x, y := readInput(filename)
+    vXmin, vXmax := 1, x[1]
+    vYmin, vYmax := y[0], (y[0] + 1) * (y[0]) / 2
+
+    hitCount := 0
+    for vX := vXmin; vX <= vXmax; vX++ {
+        for vY := vYmin; vY <= vYmax; vY++ {
+            hit := false
+            vel := [2]int{vX, vY}
+            pos := [2]int{0, 0}
+            for {
+                if x[0] <= pos[0] && pos[0] <= x[1] && y[0] <= pos[1] && pos[1] <= y[1] {
+                    hit = true
+                    break
+                }
+                if pos[0] > x[1] || pos[1] < y[0] {
+                    break
+                }
+                pos = [2]int{pos[0] + vel[0], pos[1] + vel[1]}
+
+                if vel[0] > 0 {
+                    vel[0]--
+                    vel[1]--
+                } else if vel[0] < 0 {
+                    vel[0]++
+                    vel[1]--
+                } else {
+                    vel[1]--
+                }
+            }
+            if hit {
+                hitCount++
+            }
+
         }
     }
-    maxY  := (minY + 1) * (minY) / 2
-    fmt.Print(maxY)
+    fmt.Printf("PART 2 Hit Count: %v.\n", hitCount)
+    return hitCount
 }
 
 func readInput(filename string) ([2]int, [2]int) {
