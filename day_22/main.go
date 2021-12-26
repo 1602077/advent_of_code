8766a3ab8aa9859a6526aa77b2b81d8758bddf78
1602077 <62025739+1602077@users.noreply.github.com>
2021-12-26 21:08:22 +0000

    complete day_22

--- day_22/main.go
+++ day_22/main.go
@@ -7,18 +7,35 @@ import (
     "strings"
 )
 
+type Point struct {
+    x, y, z int
+}
+
 type RebootStep struct {
     state bool
     start Point
     end Point
 }
 
-type Point struct {
-    x, y, z int
+func (rs1 RebootStep) intersect(rs2 RebootStep, state bool) RebootStep {
+    return RebootStep{
+        state,
+        Point{max(rs1.start.x, rs2.start.x), max(rs1.start.y, rs2.start.y), max(rs1.start.z, rs2.start.z)},
+        Point{min(rs1.end.x, rs2.end.x), min(rs1.end.y, rs2.end.y), min(rs1.end.z, rs2.end.z)},
+    }
+}
+
+func (rs RebootStep) isValid() bool {
+    return rs.start.x <= rs.end.x && rs.start.y <= rs.end.y && rs.start.z <= rs.end.z
+}
+
+func (rs RebootStep) vol() int {
+    return (abs(rs.end.x-rs.start.x) + 1) * (abs(rs.end.y-rs.start.y) + 1) * (abs(rs.end.z - rs.start.z) + 1)
 }
 
 func main() {
     part1("./input.txt")
+    part2("./input.txt")
 }
 
 func part1(filename string) int {
@@ -43,6 +60,35 @@ func part1(filename string) int {
     }
     fmt.Printf("PART 1 Number of cubes on: %v.\n", numCubes)
     return numCubes
+}
+
+func part2(filename string) int {
+    RebootSteps := readInput(filename, false)
+    list := []RebootStep{}
+
+    for _, rs1 := range RebootSteps {
+        add := []RebootStep{}
+        if rs1.state {
+            add = append(add, rs1)
+        }
+        for _, rs2 := range list {
+            if intersection := rs1.intersect(rs2, !rs2.state); intersection.isValid() {
+                add = append(add, intersection)
+            }
+        }
+        list = append(list, add...)
+    }
+
+    count := 0
+    for _, rs := range list {
+        if rs.state {
+            count += rs.vol()
+        } else {
+            count -= rs.vol()
+        }
+    }
+    fmt.Printf("PART 2 Number of cubes that are on: %v.", count)
+    return count
 }
 
 func readInput(filename string, limitRange bool) []RebootStep {
@@ -95,3 +141,9 @@ func min(a, b int) int {
     return b
 }
 
+func abs(a int) int {
+    if a > 0 {
+        return a
+    }
+    return -a
+}
